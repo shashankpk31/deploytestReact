83c94036041e89f1071941ee371e627a530f301f
shashankpk31 <shashankpk34@gmail.com>
2021-11-01 18:53:29 +0530

    Updates

---
+++ precache-manifest.8f5baa4056761d9b6998cd87d771ad59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5fa121278f025f86f87e8ea56aa236",
+    "url": "/deploytestReact/index.html"
+  },
+  {
+    "revision": "34ac1c4ca308cc4cf64f",
+    "url": "/deploytestReact/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "34ac1c4ca308cc4cf64f",
+    "url": "/deploytestReact/static/js/2.ce399cc2.chunk.js"
+  },
+  {
+    "revision": "c75109d92490c1e699de",
+    "url": "/deploytestReact/static/js/main.786aa983.chunk.js"
+  },
+  {
+    "revision": "35125612006b97f5b6f4",
+    "url": "/deploytestReact/static/js/runtime~main.aa8f78a3.js"
+  },
+  {
+    "revision": "62004a8742210fa08f985e68aaeedacd",
+    "url": "/deploytestReact/static/media/cong.62004a87.jpg"
+  }
+]);
